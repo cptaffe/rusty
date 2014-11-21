3c8c6a4d4c5ef2324d3e70e372ceb2831aa50d86
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-20 20:56:04 -0600

    buildable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// stuff with rust
+
 fn main() {
-    println!("Hello, world!")
+
+	let mut x = 3i;
+
+	x = if x == 2 {
+		5
+	} else {
+		println!("Hello");
+		4
+	};
+
+	println!("x is {}", x)
 }
